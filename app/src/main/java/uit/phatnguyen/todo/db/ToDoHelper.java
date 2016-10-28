e169aaed7881c0fd41c605e195e7eb588400424e
phatntuit <phatpropk@gmail.com>
2016-10-29 01:03:28 +0700

    Hello 29-10-2016

--- app/src/main/java/uit/phatnguyen/todo/db/ToDoHelper.java
+++ app/src/main/java/uit/phatnguyen/todo/db/ToDoHelper.java
@@ -1,27 +1,89 @@
 package uit.phatnguyen.todo.db;
 
-import android.content.Context;
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteOpenHelper;
+
+import uit.phatnguyen.todo.model.Todo;
+import uit.phatnguyen.todo.model.TodoList;
 
 /**
- * Created by PhatNguyen on 2016-10-26.
+ * Created by PhatNguyen on 2016-10-27.
  */
 
-public class ToDoHelper extends SQLiteOpenHelper{
+public class ToDoHelper {
+    private final static  String DATABASE_NAME = "TODO.sqlite";
+    private  static SQLiteDatabase database;
+    private Activity context;
 
-    public ToDoHelper(Context context, String name,
-                      SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public ToDoHelper(Activity context) {
+        this.context = context;
+        database = MySQLiteDB.initDatabase(context,DATABASE_NAME);
     }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-
+    public static SQLiteDatabase getDatabase() {
+        return database;
     }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+    public Cursor getQuery(String sql,String[] params){
+        Cursor cursor;
+        if (params == null){
+            cursor = database.rawQuery(sql,null);
+        }
+        cursor = database.rawQuery(sql,params);
+        return  cursor;
     }
+    public void executeQuery(String sql,String[] params){
+        database.execSQL(sql,params);
+    }
+    public long insertToDoList(TodoList tdl){
+        ContentValues contentValues = new ContentValues();
+        //Put các trường vào
+        contentValues.put(TodoList.COL_ID,tdl.getID());
+        contentValues.put(TodoList.COL_TITLE,tdl.getTITLE());
+        contentValues.put(TodoList.COL_COLOR,tdl.getCOLOR());
+        contentValues.put(TodoList.COL_NGAYTAO,tdl.getNGAYTAO());
+        contentValues.put(TodoList.COL_NGAYSUA,tdl.getNGAYSUA());
+
+        return database.insert(TodoList.TABLE_NAME,null,contentValues);
+    }
+    public long insertToDoItem(Todo todo){
+        ContentValues contentValues = new ContentValues();
+        //Put các trường vào
+        contentValues.put(Todo.COL_ID,todo.getID());
+        contentValues.put(Todo.COL_TODOFK,todo.getTODO_FK());
+        contentValues.put(Todo.COL_CONTENT,todo.getCONTENT());
+        contentValues.put(Todo.COL_DATE,todo.getDATE());
+        contentValues.put(Todo.COL_HOUR,todo.getHOUR());
+        contentValues.put(Todo.COL_LOCATION,todo.getLOCATION());
+        contentValues.put(Todo.COL_STATUS,todo.getSTATUS());
+        contentValues.put(Todo.COL_COLOR,todo.getCOLOR());
+        contentValues.put(Todo.COL_NGAYTAO,todo.getNGAYTAO());
+        contentValues.put(Todo.COL_NGAYSUA,todo.getNGAYSUA());
+
+        return database.insert(Todo.TABLE_NAME,null,contentValues);
+    }
+    public int getNext(String table_name, String col_integer){
+        String query = "SELECT " + col_integer + " FROM " + table_name + " ORDER BY "+ col_integer
+                +" DESC LIMIT 1";
+        System.out.println(query);
+        Cursor cursor = getQuery(query,null);
+        cursor.moveToFirst();
+        if(cursor.isNull(0))
+            return 0;
+        else
+            return cursor.getInt(0) + 1;
+    }
+    /*
+    for loop data use cursor
+            for(int i = 0; i < cursor.getCount(); i++){
+            cursor.moveToPosition(i);
+            int id = cursor.getInt(0);
+            String ten = cursor.getString(1);
+            String sdt = cursor.getString(2);
+            byte[] anh = cursor.getBlob(3);
+            list.add(new NhanVien(id, ten, sdt, anh));
+        }
+     */
+
+
 }
--- app/src/main/java/uit/phatnguyen/todo/model/Todo.java
+++ app/src/main/java/uit/phatnguyen/todo/model/Todo.java
@@ -5,4 +5,133 @@ package uit.phatnguyen.todo.model;
  */
 
 public class Todo {
+    private int ID;
+    private String TODO_FK;
+    private String CONTENT;
+    private String DATE;
+    private String HOUR;
+    private String LOCATION;
+    private int STATUS; //1 LA HOAN THANH
+    private String COLOR;
+    private String NGAYTAO;
+    private String NGAYSUA;
+
+    //Khai bao truong cua cac cot trong CSDL;
+    public final static String COL_ID = "ID";
+    public final static String COL_TODOFK = "TODO_FK";
+    public final static String COL_CONTENT = "CONTENT";
+    public final static String COL_DATE = "DATE";
+    public final static String COL_HOUR = "TIME";
+    public final static String COL_LOCATION = "LOCATION";
+    public final static String COL_STATUS = "STATUS";
+    public final static String COL_COLOR = "COLOR";
+    public final static String COL_NGAYTAO = "NGAYTAO";
+    public final static String COL_NGAYSUA = "NGAYSUA";
+
+    public final static String TABLE_NAME = "TODOITEMS";
+
+    public Todo() {
+    }
+
+    public Todo(int ID, String TODO_FK, String CONTENT, String DATE, String HOUR,
+                String LOCATION, int STATUS, String COLOR, String NGAYTAO, String NGAYSUA) {
+        this.ID = ID;
+        this.TODO_FK = TODO_FK;
+        this.CONTENT = CONTENT;
+        this.DATE = DATE;
+        this.HOUR = HOUR;
+        this.LOCATION = LOCATION;
+        this.STATUS = STATUS;
+        this.COLOR = COLOR;
+        this.NGAYTAO = NGAYTAO;
+        this.NGAYSUA = NGAYSUA;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getTODO_FK() {
+        return TODO_FK;
+    }
+
+    public void setTODO_FK(String TODO_FK) {
+        this.TODO_FK = TODO_FK;
+    }
+
+    public String getCONTENT() {
+        return CONTENT;
+    }
+
+    public void setCONTENT(String CONTENT) {
+        this.CONTENT = CONTENT;
+    }
+
+    public String getDATE() {
+        return DATE;
+    }
+
+    public void setDATE(String DATE) {
+        this.DATE = DATE;
+    }
+
+    public String getHOUR() {
+        return HOUR;
+    }
+
+    public void setHOUR(String HOUR) {
+        this.HOUR = HOUR;
+    }
+
+    public String getLOCATION() {
+        return LOCATION;
+    }
+
+    public void setLOCATION(String LOCATION) {
+        this.LOCATION = LOCATION;
+    }
+
+    public int getSTATUS() {
+        return STATUS;
+    }
+
+    public void setSTATUS(int STATUS) {
+        this.STATUS = STATUS;
+    }
+
+    public String getCOLOR() {
+        return COLOR;
+    }
+
+    public void setCOLOR(String COLOR) {
+        this.COLOR = COLOR;
+    }
+
+    public String getNGAYTAO() {
+        return NGAYTAO;
+    }
+
+    public void setNGAYTAO(String NGAYTAO) {
+        this.NGAYTAO = NGAYTAO;
+    }
+
+    public String getNGAYSUA() {
+        return NGAYSUA;
+    }
+
+    public void setNGAYSUA(String NGAYSUA) {
+        this.NGAYSUA = NGAYSUA;
+    }
+
+    @Override
+    public String toString() {
+        String todo = "";
+        todo += "TASK : " + this.getID() ;
+        todo += "--" + "CONTENT : " + this.getCONTENT();
+        return todo;
+    }
 }
